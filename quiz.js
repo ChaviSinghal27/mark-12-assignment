939905e71ae7c3d88ad36df5fd30caa9f7fc33ec
ChaviSinghal27 <108614142+ChaviSinghal27@users.noreply.github.com>
2022-07-20 13:45:26 +0530

    triangle quiz complete

---
+++ quiz.js
@@ -0,0 +1,24 @@
+const quizForm = document.querySelector("#quiz-form");
+const submitButton = document.querySelector("#submit-btn");
+const outputEl = document.querySelector("#output");
+
+const correctAnswers = ["90°", "right angled"];
+
+submitButton.addEventListener("click", calculateScore);
+
+function calculateScore() {
+    let score = 0;
+    let index = 0;
+
+    const formResults = new FormData(quizForm)
+    for(let value of formResults.values()) {
+     if(value === correctAnswers[index]){
+        score = score + 1 ;
+     } 
+
+     index = index + 1 ;
+    
+} 
+  outputEl.innerText = "your score is " + score ;
+}
+
