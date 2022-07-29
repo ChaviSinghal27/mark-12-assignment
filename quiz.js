959f54a5e5142472c1fd46831fad56b585d60479
ChaviSinghal27 <108614142+ChaviSinghal27@users.noreply.github.com>
2022-07-29 15:20:18 +0530

    css done

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector("#quiz-form");
 const submitButton = document.querySelector("#submit-btn");
 const outputEl = document.querySelector("#output");
 
-const correctAnswers = ["90°", "right angled"];
+const correctAnswers = ["90°", "right angled", "one right angle", "12, 16, 20", "Equilateral triangle"];
 
 submitButton.addEventListener("click", calculateScore);
 
